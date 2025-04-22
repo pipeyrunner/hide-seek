6c9edde0921e1fa766d632583b43316aae5383cd
TeakIvy <legocjones@gmail.com>
2025-04-22 19:08:34 -0400

    Add homepage styles and font files for 'VAG Rounded Next'

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,62 @@
-import type { ReactNode } from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import React from 'react';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
+import Link from '@docusaurus/Link';
+import './Home.css';
+import './font/stylesheet.css';
 
-import styles from './index.module.css';
-
-function HomepageHeader() {
-	const { siteConfig } = useDocusaurusContext();
+export default function Home() {
 	return (
-		<header className={clsx('hero hero--primary', styles.heroBanner)}>
-			<div className='container'>
-				<Heading as='h1' className='hero__title'>
-					{siteConfig.title}
-				</Heading>
-				<p className='hero__subtitle'>{siteConfig.tagline}</p>
-				<div className={styles.buttons}>
-					<Link
-						className='button button--secondary button--lg'
-						to='/docs/quick_start_guide'
+		<Layout description='A real-world game played on public transit'>
+			<div className='homepage-hero'>
+				<div className='hide-seek-logo'>
+					<div className='logo'>
+						<img src='/img/jetlag.png' alt='Hide and Seek Logo' />
+					</div>
+					<div
+						className='logo-container'
+						style={{ width: 'fit-content', margin: '0 auto' }}
 					>
-						Rule Book ➡
-					</Link>
+						<h1 className='title'>
+							HIDE<span className='plus'>+</span>SEEK
+						</h1>
+						<div className='line'></div>
+						<div className='subline-container' style={{}}>
+							<span className='subline left'>a transit game</span>
+							<span className='subline right'>find your friends</span>
+						</div>
+					</div>
+
+					<p className='tagline'>
+						A real-world game
+						<br />
+						played on public transit
+					</p>
+
+					<div className='badges'>
+						<span className='badge'>AGES 14+</span>
+						<span className='badge'>2–4+ PLAYERS</span>
+					</div>
+
+					<div
+						className='button-container'
+						style={{
+							display: 'flex',
+							// backgroundColor: 'red',
+							justifyContent: 'space-evenly',
+						}}
+					>
+						<Link className='button' to='/docs/quick_start_guide'>
+							Rule Book
+						</Link>
+						<Link className='button' to='/deck'>
+							Hider Deck
+						</Link>
+						<Link className='button' to='/deck'>
+							Investigation Book
+						</Link>
+					</div>
 				</div>
 			</div>
-		</header>
-	);
-}
-
-export default function Home(): ReactNode {
-	const { siteConfig } = useDocusaurusContext();
-	return (
-		<Layout description='Description will go into a meta tag in <head />'>
-			<HomepageHeader />
-			<main>{/* <HomepageFeatures /> */}</main>
 		</Layout>
 	);
 }
