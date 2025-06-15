b311c40d7630c81aeaeb4277c2ef03b8fda1ed4f
pipeyrunner <215211755+pipeyrunner@users.noreply.github.com>
2025-06-15 22:17:03 +0200

    Update disclaimer.tsx

--- src/pages/disclaimer.tsx
+++ src/pages/disclaimer.tsx
@@ -11,30 +11,8 @@ export default function Disclaimer() {
 				<h1>Disclaimer</h1>
 				
 				<p>
-					It is not endorsed by or
-					associated with the creators of the original product.
-				</p>
-
-				<p>
-					This website is <strong>not for sale</strong>,{' '}
-					<strong>not monetized</strong>, and does not offer the game or any
-					physical products for purchase. It is not intended to replace the
-					original game. All rights to the game design, branding, content, and
-					intellectual property belong entirely to Wendover Productions LLC and
-					their partners.
-				</p>
-
-				<p>
-					If you represent Wendover Productions or Jet Lag: The Game and would
-					like this site modified or removed, please contact me at{' '}
-					<a href='mailto:collinj2055@gmail.com'>collinj2055@gmail.com</a>. I am
-					happy to cooperate fully.
-				</p>
-
-				<p style={{ marginTop: '2rem', fontStyle: 'italic', color: '#666' }}>
-					This project was created out of admiration for the game and its
-					creators, and is meant to support and encourage real-world play and
-					interest in the official version.
+				Мы лишь адаптировали уже существующую игру на русский язык и перевели ее с империальной системы измерений в метрическую. Перевод был с юморком, чтобы инструкция и игра не были душными простынями написанными юристами
+				
 				</p>
 			</main>
 		</Layout>
