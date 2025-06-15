a8225462581aee523c348f79a4283599123feeea
pipeyrunner <215211755+pipeyrunner@users.noreply.github.com>
2025-06-15 15:39:23 +0200

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-	title: 'Jet Lag The Game',
+	title: 'Jet Lag',
 	tagline: 'Hide and Seek',
 	favicon: 'img/favicon.ico',
 
