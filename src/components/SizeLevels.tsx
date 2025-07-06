2380c59963a691607d05afbe91e3f554d7ec659a
pipeyrunner <215211755+pipeyrunner@users.noreply.github.com>
2025-07-07 00:22:30 +0200

    Update SizeLevels.tsx

--- src/components/SizeLevels.tsx
+++ src/components/SizeLevels.tsx
@@ -79,7 +79,7 @@ const SizeLevels: React.FC<SizeLevelsProps> = ({
 }) => {
     const sizes = [
         {
-            label: 'ААААА',
+            label: 'SMALL',
             color: '#ffffff',
             bgColor: '#ffc800',
             content: small,
